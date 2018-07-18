4e4e55cd6e6af6e69716e786bf6b68abfac1aa76
hms37 <h.shah5393@gmail.com>
2018-07-18 14:15:31 -0400

    Update primeFactors.js

--- primeFactors.js
+++ primeFactors.js
@@ -1,10 +1,14 @@
+// Unique set of Prime Factors
+
 function primeFactors(n) {
 var factors = [];
+var exists = {};
 var divider = 2;
 while(n > 2) {
-    if (n % divider == 0) {
-        factors.push(divider);
-        n = n/divider;
+    if (n % divider == 0 && !exists[divider]) {
+            factors.push(divider);
+            n = n/divider;
+            exists[divider]=true;
     }
     else {
         divider++
